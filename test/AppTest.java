9f832a18014fed13fdc7b84eee0492319bd63f70
Kavya-original <kavyamalleboina0@gmail.com>
2026-03-26 13:41:37 +0530

    Initial commit of Java project

---
+++ test/AppTest.java
@@ -0,0 +1,12 @@
+public class AppTest {
+    public static void main(String[] args) {
+        String expected = "Hello";
+        String actual = "Hello";
+
+        if (expected.equals(actual)) {
+            System.out.println("Test Passed!");
+        } else {
+            System.out.println("Test Failed!");
+        }
+    }
+}
